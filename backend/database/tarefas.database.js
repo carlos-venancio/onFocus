e0de6b317b9271ed94c76e3c831c29587f9f1b86
Carlos Henrique <hencarlos42@gmail.com>
2024-02-20 17:28:01 -0300

    performance: colocando a inicialização do banco em uma função

--- backend/database/tarefas.database.js
+++ backend/database/tarefas.database.js
@@ -1,24 +1,24 @@
 import { openDatabase } from 'react-native-sqlite-storage';
 
+export default async function iniciarBanco() {
 
-// 1. Criaçaõ do banco de dados
-const db = await openDatabase({
-    name: 'onfocus'
-})
+    // 1. Criação do banco de dados
+    const db = await openDatabase({
+        name: 'onfocus'
+    })
 
-// 2. Criação das tabela
-db.transaction((tx) => {
-    tx.executeSql(`CREATE TABLE tarefas(
-                    pk_tarefaId INTEIRO PRIMARY KEY NOT NULL,
-                    tituloTarefa TEXT NOT NULL,
-                    dataInicio TEXT NOT NULL,
-                    dataFinal TEXT NOT NULL,
-                    desc TEXT NOT NULL,
-                    status TEXT NOT NULL 
-        );`, // O status deve ser "concluído", "cancelada" ou "pendente", "em breve"
-        [],
-        () => console.info("Tabela tarefas criada com sucesso"),
-        error => console.error("Erro ao criar tabela de tarefas: " + error))  
-})
-
-export default db;
+    // 2. Criação das tabelas
+    db.transaction((tx) => {
+        tx.executeSql(`CREATE TABLE tarefas(
+                        pk_tarefaId INTEIRO PRIMARY KEY NOT NULL,
+                        tituloTarefa TEXT NOT NULL,
+                        dataInicio TEXT NOT NULL,
+                        dataFinal TEXT NOT NULL,
+                        desc TEXT NOT NULL,
+                        status TEXT NOT NULL 
+            );`, // O status deve ser "concluído", "cancelada" ou "pendente", "em breve"
+            [],
+            () => console.info("Tabela tarefas criada com sucesso"),
+            error => console.error("Erro ao criar tabela de tarefas: " + error))  
+    })
+}
