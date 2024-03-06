0db8897e889488dacca7ef79f214207c1ae46b1c
BrennonMeireles <brennonmeirles7@gmail.com>
2024-03-06 18:36:14 -0300

    feat: mudando para react-native-sqlite-storage

--- backend/database/initDB.js
+++ backend/database/initDB.js
@@ -1,33 +1,35 @@
-import sqlite from 'sqlite3'
+import Sqlite from "react-native-sqlite-storage";
 
-// habilitar logs detalhados 
-const sqlite3 = sqlite.verbose()
+Sqlite.enablePromise(true);
 
-export default async function iniciarBanco() {
-  
-  // abrir a conexão com o banco de dados (cria caso não exista) 
-  const db = new sqlite3.Database(`${import.meta.dirname}\\onFocus.db`,(err) => {
-    // import.meta.dirname é usado para saber o nome do diretório atual
-      
-      if (err) return console.error(err.message);
-  
-      console.log('Connected to the SQlite database.');
-    })
-  
-  // criar a tabela de tarefas de maneira sincrona se ela não existir
-  db.serialize(() => {
-      db.run(`CREATE TABLE IF NOT EXISTS tarefas(
-          pk_tarefaId INTEGER PRIMARY KEY AUTOINCREMENT,
-          tituloTarefa TEXT,
-          dataInicio TEXT,
-          duracao TEXT,
-          descricao TEXT,
-          status TEXT
-      )`);
-      console.log('Table created with sucess')
+export const getDBConnection = async () => {
+  // abrir a conexão com o banco de dados (cria caso não exista)
+  const db = await Sqlite.openDatabase({
+    name: "todo-data.db",
+    location: "default",
   });
-  
-  console.log('initDB finish')
 
-  return db
-}
+  // cria a tabela tarefas caso el não exista
+  await db.transaction((tx) => {
+    tx.executeSql(
+      `CREATE TABLE IF NOT EXISTS tarefas(
+                pk_tarefaId INTEGER PRIMARY KEY AUTOINCREMENT,
+                tituloTarefa TEXT,
+                dataInicio TEXT,
+                duracao TEXT,
+                descricao TEXT,
+                status TEXT
+            )`,
+      [],
+      () => console.log('Tabela "tarefas" criada com sucesso'),
+      (err) => console.error('Erro ao criar a tabela "tarefas":', err)
+    );
+  });
+
+  return async (statement, params, sucess, error) => {
+    await db.transaction((tx) => {
+      tx.executeSql(statement, params, sucess, error);
+    })
+    await db.close()
+  };
+};
--- backend/repositories/tarefas.database.js
+++ backend/repositories/tarefas.database.js
@@ -1,41 +1,39 @@
-import iniciarBanco from "../database/initDB.js";
+import { getDBConnection } from "../database/initDB.js";
 
 // inicia conexão com o banco de dados
-const db = await iniciarBanco();
+const execute = await getDBConnection();
 
 // vai exportar funções para serem utilizadas nos controladores
 export default {
   // Função para buscar todas as tarefas
-  getAllTarefas: function (callback) {
-    db.all("SELECT tituloTarefa, dataInicio FROM tarefas", (err, rows) => {
-      if (err) {
+  getAllTarefas: async function (callback) {
+    return await execute(
+      "SELECT tituloTarefa, dataInicio FROM tarefas",
+      [], // efeito (resposta) logo após a execução da função
+      (rows) => callback(null, rows),
+      (err) => {
         console.error(err.message);
         return callback(err);
-      }
-      // efeito (resposta) logo após a execução da função
-      callback(null, rows);
-    });
-  },
-
-  // Função para buscar todos os dados de apenas uma tarefa
-  getOneTarefa: function (id, callback) {
-    db.get(
-      `SELECT tituloTarefa, dataInicio, duracao, descricao, status FROM tarefas WHERE pk_tarefaId = ?`,
-      [id],
-      (err, row) => {
-        if (err) {
-          console.error(err.message);
-          return callback(err);
-        }
-        // retorna a tarefa(row)
-        callback(null, row);
       }
     );
   },
 
+  // Função para buscar todos os dados de apenas uma tarefa
+  getOneTarefa: async function (id, callback) {
+    return await execute(
+      `SELECT tituloTarefa, dataInicio, duracao, descricao, status FROM tarefas WHERE pk_tarefaId = ?`,
+      [id],
+      (row) => callback(null, row),
+      (err) => {
+        console.error(err.message);
+        return callback(err);
+      }
+    )
+  },
+
   // funcao para adicionar uma nova tarefa
   addTarefa: async function (tarefa, callback) {
-    db.run(
+    return await execute(
       `INSERT INTO tarefas (tituloTarefa, dataInicio, duracao, descricao, status) VALUES (?, ?, ?, ?, ?)`,
       [
         tarefa.tituloTarefa,
@@ -44,67 +42,58 @@ export default {
         tarefa.descricao,
         tarefa.status,
       ],
+      () => callback(null, { id: this.lastID }),
       function (err) {
         if (err) {
           console.error(err.message);
           return callback(err);
         }
-        // efeito (resposta) logo após a execução da função
-        callback(null, { id: this.lastID });
       }
     );
   },
 
   // função para alterar uma teraf pelo ID
   alterTarefa: async function (id, tarefa, callback) {
-    db.run(
+    return await execute(
       "UPDATE tarefas SET tituloTarefa = ?, dataInicio = ?, duracao = ?,descricao = ?, status = ? WHERE pk_tarefaId = ?",
       [...Object.values(tarefa), id],
+      () => callback(null, { lineChanges: this.changes, newTask: tarefa }),
       function (err) {
-        if (err) {
-          console.error(err.message);
-          return callback(err);
-        }
-        // efeito (resposta) logo após a execução da função
-        callback(null, { lineChanges: this.changes, newTask: tarefa });
+        console.error(err.message);
+        return callback(err);
       }
     );
   },
 
   // funcao para excluir uma tarefa pelo ID
   deleteTarefa: async function (id, callback) {
-    db.run(`DELETE FROM tarefas WHERE pk_tarefaId = ?`, [id], function (err) {
-      if (err) {
+    return await execute(
+      `DELETE FROM tarefas WHERE pk_tarefaId = ?`,
+      [id],
+      () => callback(null, { id: this.changes ? id : 0 }),
+      function (err) {
         console.error(err.message);
         return callback(err);
-      }
-      // efeito (resposta) logo após a execução da função
-      callback(null, { id: this.changes ? id : 0 });
-    });
-  },
-
-  // funcao para atualizar o status de uma tarefa para cancelada pelo ID
-  cancelTarefa: async function (id, callback) {
-    console.log(id);
-    db.run(
-      `UPDATE tarefas SET status = 'cancelada' WHERE pk_tarefaId = ?`,
-      [id],
-      function (err) {
-        // efeito (resposta) logo após a execução da função
-        return err ? callback(err) : callback(null, { lines: this.changes });
       }
     );
   },
 
+  // funcao para atualizar o status de uma tarefa para cancelada pelo ID
+  cancelTarefa: async function (id, callback) {
+    return await execute(
+      `UPDATE tarefas SET status = 'cancelada' WHERE pk_tarefaId = ?`,
+      [id],
+      () => callback(null, { lines: this.changes }),
+      (err) => callback(err)
+    );
+  },
+
   concluirTarefa: async function (id, callback) {
-    console.log(id);
-    db.run(
+    return await execute(
       `UPDATE tarefas SET status = 'conclúido' WHERE pk_tarefaId = ?`,
       [id],
-      function (err) {
-        // efeito (resposta) logo após a execução da função
-        return err ? callback(err) : callback(null, { lines: this.changes });
-      }
+      () => callback(null, { lines: this.changes }),
+      (err) => callback(err)
     );
   },
 };
